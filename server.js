9f2cfb1182203b4eef797cd67cf23fd597fff0aa
tsmith1444 <tyler@tsmithinvestments.com>
2025-05-26 10:58:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -34,10 +34,10 @@ app.get('/api/test', (req, res) => {
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('build'));
   
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 } else {
   // Simple route for testing that the API is running
