4e3d7a94678ade00985c7a8cadb772f1d5c575e0
tsmith1444 <tyler@tsmithinvestments.com>
2025-05-25 22:53:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -219,6 +219,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'AI Spouse API is running' });
 });
 
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
